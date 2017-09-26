ee2a927f738d552f1abf36ee87b9865d1b5fc580
ikaros <1371689491@qq.com>
2017-09-26 09:40:02 +0800

    18

---
+++ mtsleep1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import thread
+from time import sleep, ctime
+
+def loop0():
+    print 'start loop 0 at: ', ctime()
+    sleep(3)
+    print 'loop 0 done at: ', ctime()
+
+def loop1():
+    print 'start loop 1 at: ', ctime()
+    sleep(1)
+    print 'loop 1 done at: ', ctime()
+
+def main():
+    print 'starting at: ', ctime()
+    thread.start_new_thread(loop0, ())
+    thread.start_new_thread(loop1, ())
+    sleep(5)
+    print 'all DONE at: ', ctime()
+
+if __name__ == '__main__':
+    main()
